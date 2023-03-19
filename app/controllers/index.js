e5c2fe9c2e12afa084da08ac08071957a69d6270
GaspardPierre <pierredillard@gmail.com>
2023-03-19 17:05:34 +0100

    add oeuvreRouter + doc

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,7 +1,7 @@
 const utilisateurController = require ("./utilisateurController.js");
-const oeuvreController = require ("./oeuvreController.js");
+const workController = require ("./workController.js");
 const categoryController = require ("./categoryController.js");
 const loginController = require ("./loginController.js");
 
 
-module.exports =  {utilisateurController,oeuvreController,categoryController,loginController};
+module.exports =  {utilisateurController,workController,categoryController,loginController};
