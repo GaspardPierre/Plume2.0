a02b957112bda5d38448bd01bd671dd2c5e11b49
GaspardPierre <pierredillard@gmail.com>
2023-05-05 15:15:16 +0200

    style Poems.jsx

--- front/src/components/Card/Card.jsx
+++ front/src/components/Card/Card.jsx
@@ -1,21 +1,19 @@
 import React from 'react';
 import './Card.scss';
 
-export default function Card  ({ title, excerpt, onClick })  { 
 
-  
-        return (
-          <div className="col-md-4" >
-            <div className="card text-center" >
-              <div className="card-body d-flex flex-column justify-content-center">
-                <h5 className="card-title">{title}</h5>
-                <p className="card-text">{excerpt}</p>
-                <button className="btn btn-primary" onClick={onClick}>
-                  Afficher plus
-                </button>
-              </div>
-            </div>
-          </div>
-        );
-      };
-      
+export default function Card({ title, excerpt, onClick }) {
+  return (
+    <div className="col-md-4">
+      <div className="card text-center" >
+        <div className="card-body d-flex flex-column justify-content-center">
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">{excerpt}</p>
+          <button className="btn-custom" onClick={onClick}>
+            Afficher plus
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
--- front/src/components/Poems/Poems.jsx
+++ front/src/components/Poems/Poems.jsx
@@ -80,6 +80,84 @@ export default function Poems() {
           Avec sa chair et son sang,
           Il a bâti un pont.`,
       author: "Pierre Dillard"
+    },
+    {
+      id: 4,
+      title: "L'espoir",
+      content:
+        `Dans les ténèbres les plus épaisses,
+         L'espoir s'est levé,
+         Comme un triomphant soleil,
+         Aux ailes protectrices,
+          L'Espoir a dit "ne retiens pas,
+          Il a jailli comme un éclair,
+          Dans une nuit de tempête,
+          Il s'est souvenu...
+          L'Espoir a ouvert grand ses bras,
+          Il a regardé au-delà,
+          il a dit "me voici!
+          Et sur le bois il s'est offert,
+          Il a déversé la vie des cieux sur la terre,
+          Il a détrempé mon cœur de ses flots,
+          Il a ouvert une brèche,
+          Et sa lumière est devenue un feu,
+          Brûlant comme un flambeau puissant,
+          Du gouffre vers l'éternité,
+          Avec sa chair et son sang,
+          Il a bâti un pont.`,
+      author: "Pierre Dillard"
+    },
+    {
+      id: 5,
+      title: "L'espoir",
+      content:
+        `Dans les ténèbres les plus épaisses,
+         L'espoir s'est levé,
+         Comme un triomphant soleil,
+         Aux ailes protectrices,
+          L'Espoir a dit "ne retiens pas,
+          Il a jailli comme un éclair,
+          Dans une nuit de tempête,
+          Il s'est souvenu...
+          L'Espoir a ouvert grand ses bras,
+          Il a regardé au-delà,
+          il a dit "me voici!
+          Et sur le bois il s'est offert,
+          Il a déversé la vie des cieux sur la terre,
+          Il a détrempé mon cœur de ses flots,
+          Il a ouvert une brèche,
+          Et sa lumière est devenue un feu,
+          Brûlant comme un flambeau puissant,
+          Du gouffre vers l'éternité,
+          Avec sa chair et son sang,
+          Il a bâti un pont.`,
+      author: "Pierre Dillard"
+    },
+    {
+      id: 6,
+      title: "L'espoir",
+      content:
+          `Dans les ténèbres les plus épaisses,
+          L'espoir s'est levé,
+          Comme un triomphant soleil,
+          Aux ailes protectrices,
+          L'Espoir a dit "ne retiens pas,
+          Il a jailli comme un éclair,
+          Dans une nuit de tempête,
+          Il s'est souvenu...
+          L'Espoir a ouvert grand ses bras,
+          Il a regardé au-delà,
+          il a dit "me voici!
+          Et sur le bois il s'est offert,
+          Il a déversé la vie des cieux sur la terre,
+          Il a détrempé mon cœur de ses flots,
+          Il a ouvert une brèche,
+          Et sa lumière est devenue un feu,
+          Brûlant comme un flambeau puissant,
+          Du gouffre vers l'éternité,
+          Avec sa chair et son sang,
+          Il a bâti un pont.`,
+      author: "Pierre Dillard"
     }
   ];
 
@@ -89,8 +167,9 @@ export default function Poems() {
   };
 
   return (
-    <div className="container-fluid">
+    <div className="container-fluid-custom">
       <div className="row justify-content-center">
+    
         {poems.map((poem) => (
           <Card
             key={poem.id}
@@ -100,5 +179,6 @@ export default function Poems() {
         ))}
       </div>
     </div>
+  
   );
 }
