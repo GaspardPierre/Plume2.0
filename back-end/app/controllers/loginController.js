d8a2f14c9ab1178cbecb10acc6c88df68218fdcc
GaspardPierre <pierredillard@gmail.com>
2023-11-12 16:32:25 +0100

    errors handling in LoginControlleur

--- back-end/app/controllers/loginController.js
+++ back-end/app/controllers/loginController.js
@@ -4,52 +4,50 @@ require("dotenv").config();
 
 const loginControlleur = {
 
-	async checkMember(req, res) {
+    async checkMember(req, res) {
+        try {
+            console.log(req.body);
+            const { email, password } = req.body;
+            const member = await memberModel.findByEmail(email);
 
-		console.log(req.body);
-		const { email, password } = req.body;
-		const member = await memberModel.findByEmail(email);
+            if (!member) {
+                return res.status(404).json({ message: "Utilisateur non trouvé" });
+            }
 
-		if (!member) {
-			console.log(res.json);
-			return res.status(404).json({ message: "Utilisateur non trouvé" });
-		}
-		// fonction qui permet de comparer le mot de passe et le mot de passe stocqué dans la BDD pour l'itilisateur trouvé.
+            const isPasswordValid = await bcrypt.compare(password, member.password);
 
-		const isPasswordValid = await bcrypt.compare(
-			password,
-			member.password
-		);
+            if (!isPasswordValid) {
+                return res.status(401).json({ message: "Mot de passe incorrect" });
+            }
 
-		if (!isPasswordValid) {
-			return res.status(401).json({ message: "Mot de passe incorrect" });
-		}
-		// On  retire le mdp de l'objet utilisateur pas de la BDD.
-		delete member.password;
-if(member.id){
-		req.session.user ={ id : member.id};
-		req.session.role = member.role
-		console.log(`Après connexion, req.session: ${JSON.stringify(req.session)}`);
-}
-		res.status(200).json({ member });
-	},
+            delete member.password;
 
+            if (member.id) {
+                req.session.user = { id: member.id };
+                req.session.role = member.role;
+                console.log(`Après connexion, req.session: ${JSON.stringify(req.session)}`);
+            }
 
- async logout (req, res) {
-	// Destroy user session
-	req.session.destroy((err) => {
-	  if (err) {
-	
-		console.error(err);
-		res.status(500).send({ error: "Failed to logout" });
-	  } else {
-	
-		res.status(200).send({ message: "Logout successful" });
-	  }
-	});
-  }
+            res.status(200).json({ member });
+        } catch (err) {
+            console.error(err);
+            res.status(500).json({ error: "Une erreur est survenue lors de la vérification de l'utilisateur." });
+        }
+    },
+
+    async logout(req, res) {
+        try {
+            req.session.destroy((err) => {
+                if (err) {
+                    throw err;
+                }
+                res.status(200).send({ message: "Logout successful" });
+            });
+        } catch (err) {
+            console.error(err);
+            res.status(500).send({ error: "Failed to logout" });
+        }
+    }
 };
-
-
 
 module.exports = loginControlleur;
--- back-end/index.js
+++ back-end/index.js
@@ -1,24 +1,29 @@
-// CONFIGURATION DU SERVEUR***************************
-require('dotenv').config();
 const express = require('express');
 const router = require('./app/router');
-const app = express();
-let session = require('express-session');
+const session = require('express-session');
 const cors = require('cors');
-// L’import “morgan” sert à enregistrer les demandes HTTP d’une application Node.js pour le débogage et le suivi des erreurs.
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
 
+// Configuration des variables d'environnement en fonction de l'environnement
+if (process.env.NODE_ENV === 'production') {
+    dotenv.config({ path: '.env.production' });
+} else {
+    dotenv.config({ path: '.env.development' });
+}
+
+const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(session( {
+app.use(session({
     secret: process.env.SESSION_SECRET,
-    resave: true, //sauvegarde la session à chaque changement
+    resave: true,
     saveUninitialized: true,
     cookie: {
         maxAge: 2013210,
-        sameSite:false,
-        httpOnly:false
+        sameSite: false,
+        httpOnly: false
     }
 }));
 
@@ -28,29 +33,22 @@ app.use(morgan('dev'));
 const corsOptions = {
     origin: ['http://localhost:5173'],
     credentials: true,
-    optionsSuccessStatus: 200   
+    optionsSuccessStatus: 200
 };
 app.use(cors(corsOptions));
 
 app.use(bodyParser.json());
-
-// facilite la récupération des données de la requête dans un objet JavaScript pour leur traitement dans l’application.
-
 app.use(bodyParser.urlencoded({
     extended: true
-  }));
+}));
 
-//   afficher dans la console du serveur les informations de la session.
-
-  app.use((req, res, next) => {
-    console.log("my Mideweware", req.session);
+app.use((req, res, next) => {
+    console.log("my Middleware", req.session);
     next();
-  });
+});
 
-  app.use('/api', router);
+app.use('/api', router);
 
-  
-
-  app.listen(port, () => {
+app.listen(port, () => {
     console.log(`http://localhost:${port}`);
-  });
+});
