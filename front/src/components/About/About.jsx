783b9f79322f0151ac1010c72cca0cff19a7055a
GaspardPierre <pierredillard@gmail.com>
2024-03-28 13:55:00 +0100

    update about layout

--- front/src/components/About/About.jsx
+++ front/src/components/About/About.jsx
@@ -7,7 +7,8 @@ export default function About() {
         <div className="row">
     <div className="col-md-6 my-4">
     <div className=" card shadow">
-    <img src={auteur} className="card-img-top" alt="Auteur"/>
+    <img src={auteur} className="card-img-top" alt="Photo de Matthieu Mouquet" />
+    
   </div>
     </div>
     <div className="col-md-6">
@@ -19,7 +20,13 @@ export default function About() {
             <p className='citation fst-italic'>Je laisse l'oiseau se poser"</p>
             <footer className="blockquote-footer">Matthieu Mouquet</footer>
           </blockquote>
-  <p className="lead lead bio-text">Matthieu est bien plus qu'un simple écrivain-poète ; il incarne la fusion entre l'émotion et les mots, transformant sa douleur en beauté littéraire. Depuis ses débuts à l'âge de 15 ans et demi, il a utilisé l'écriture comme un moyen de transcender les défis physiques et émotionnels auxquels il fait face. Son parcours, marqué par des périodes difficiles, est devenu le terreau fertile de sa créativité.Sa professeure de français fut témoin de son talent lorsqu'il écrivit : <span className="className='citation fst-italic"> "Écris ce que tu ressens, même une lettre peut suffire à stimuler ton imagination et à dissiper le brouillard en toi."</span></p><p className='lead lead bio-text'>
+
+ 
+    </div>
+  </div>
+  <div className='row'>
+    <div className='col-md-12'>
+    <p className="lead lead bio-text ">Matthieu est bien plus qu'un simple écrivain-poète ; il incarne la fusion entre l'émotion et les mots, transformant sa douleur en beauté littéraire. Depuis ses débuts à l'âge de 15 ans et demi, il a utilisé l'écriture comme un moyen de transcender les défis physiques et émotionnels auxquels il fait face. Son parcours, marqué par des périodes difficiles, est devenu le terreau fertile de sa créativité.Sa professeure de français fut témoin de son talent lorsqu'il écrivit : <span className="className='citation fst-italic"> "Écris ce que tu ressens, même une lettre peut suffire à stimuler ton imagination et à dissiper le brouillard en toi."</span></p><p className='lead lead bio-text'>
 
 Cette simple devise révèle son engagement à exprimer ses émotions à travers les mots, une philosophie qui guide chacune de ses œuvres.Dès lors, Matthieu a exploré divers genres littéraires, plongeant d'abord dans la poésie en 2021. Ses poèmes, telles des feuilles d'automne emportées par le vent, capturent la beauté éphémère de l'existence.
 
@@ -28,7 +35,9 @@ Au contraire, il les transforme en atouts, infusant ses écrits d'une sensibilit
 
 Chaque mot qu'il choisit est empreint de sincérité et de vérité, reflétant son voyage intérieur.
 Sur son site, plongez dans l'univers captivant de Matthieu, où chaque texte est une invitation à explorer les méandres de l'âme humaine et à embrasser la beauté de l'imperfection...</p>
+
     </div>
+
   </div>
       
       </div>
