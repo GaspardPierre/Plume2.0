14a63939f3f44d6169b84bb3493bd6bf3cb49aba
GaspardPierre <98175781+GaspardPierre@users.noreply.github.com>
2024-05-11 18:47:34 +0200

    Update labelRouter.js

--- app/router/labelRouter.js
+++ app/router/labelRouter.js
@@ -1,14 +1,22 @@
-const express = require('express');
-const { labelController } = require('../controllers/index');
+const express = require("express");
+const { labelController } = require("../controllers/index");
 const router = express.Router();
-const security = require('../service/security');
+const security = require("../service/security");
+const multer = require('multer');
+const upload = multer();
+const authenticateJWT = require("../service/authenticateJWT");
+
 // All routes are prefixed with /label
 
-router.get('/', labelController.getAllLabels);
-router.get('/:id', labelController.getLabel);
-router.post('/addLabel', security.checkAdmin ,labelController.addLabel);
-router.post('/addLabelToWork/:workId', security.checkAdmin, labelController.addLabelToWork);
-
-router.patch('/:id', security.checkAdmin, labelController.modifyLabel);
+router.get("/", labelController.getAllLabels);
+router.get("/:id", labelController.getLabel);
+router.post("/addLabel", authenticateJWT,upload.none(), security.checkAdmin, labelController.addLabel);
+router.post(
+  "/addLabelToWork/:workId",authenticateJWT,
+  security.checkAdmin,
+  labelController.addLabelToWork
+);
+router.patch("/:id",authenticateJWT, security.checkAdmin, labelController.modifyLabel);
+router.delete("/:id",authenticateJWT,security.checkAdmin, labelController.deleteLabel);
 
 module.exports = router;
