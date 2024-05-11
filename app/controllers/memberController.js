319b15844d55d58191bddc556cbe49f527ca4ded
GaspardPierre <98175781+GaspardPierre@users.noreply.github.com>
2024-05-11 18:51:33 +0200

    Update memberController.js

--- app/controllers/memberController.js
+++ app/controllers/memberController.js
@@ -1,45 +1,112 @@
 const { memberModel } = require("../models");
 const bcrypt = require("bcrypt");
-const jwt = require('jsonwebtoken');
-require("dotenv").config();
 
-const loginControlleur = {
+const memberController = {
 
-    async checkMember(req, res) {
-        try {
-            console.log(req.body);
-            const { email, password } = req.body;
-            const member = await memberModel.findByEmail(email);
-
-            if (!member) {
-                return res.status(404).json({ message: "Utilisateur non trouvé" });
-            }
-
-            const isPasswordValid = await bcrypt.compare(password, member.password);
-
-            if (!isPasswordValid) {
-                return res.status(401).json({ message: "Mot de passe incorrect" });
-            }
-
-            const token = jwt.sign(
-                { id: member.id, role: member.role, email: member.email,pseudo: member.pseudo},
-                process.env.JWT_SECRET,
-                { expiresIn: process.env.JWT_EXPIRATION || '1h' } // Utiliser une valeur par défaut si non spécifiée
-            );
-            
-
-    
-            res.status(200).json({ message: "Connexion réussie", token });
-        } catch (err) {
-            console.error(err);
-            res.status(500).json({ error: "Une erreur est survenue lors de la vérification de l'utilisateur." });
-        }
+    async getAllMembers(req,res) {
+      try {
+        const members = await memberModel.findAll();
+        res.json(members);
+      } catch (error) {
+        console.error(error);
+        res.status(500).send('Erreur lors de la récupération des membres');
+      }
     },
-    async logout(req, res) {
-       
-        res.status(200).send({ message: "Déconnexion réussie. Supprimez le token côté client." });
-    }
-    
-};
+    async verifyRole(req, res) {
 
-module.exports = loginControlleur;
+      console.log("requête reçue!!!!");
+   
+      if (!req.session.user || !req.session.role) {
+          console.log(`Session invalid: ${req.session.id}`);
+          return res.status(401).json({
+              error: "authentication_required",
+              message: "Authentication required or session expired",
+              detail: {
+                  userExists: !!req.session.user,
+                  roleExists: !!req.session.role
+              }
+          });
+      }
+  
+ 
+      const response = res.status(200).json({
+          member: {
+              id: req.session.user.id,
+              role: req.session.role,
+              pseudo: req.session.user.pseudo
+          },
+          sessionExpiresAt: req.session.cookie._expires  
+      });
+      console.log("Session invalid: ", req.session.id, " User: ", !!req.session.user, " Role: ", !!req.session.role);
+
+      return response;
+  }
+,  
+    async addMember(req,res) {
+        const { pseudo, email, password } = req.body;
+        console.log(req.body);
+        const member = await memberModel.findByEmail(email);
+        if (member) {
+            return res.status(401).json({ message: "Utilisateur existe déjà" });
+        }
+
+        // On génère un salt pour le mot de passe pour sécuriser le hachage , avant de le stocker en BDD
+        const salt = await bcrypt.genSalt(10);
+
+        const newMember= {
+            pseudo : pseudo,
+            email : email,
+            password : await bcrypt.hash(password, salt),
+            role : "visiteur"
+        };
+
+    const memberDb = await memberModel.insert(newMember);
+
+    res.status(200).json(memberDb);
+    },
+
+    async getMember(req,res) {
+      const member = await memberModel.findById(req.params.id);
+        
+        if(!member) {
+            res.status(404).json({ message: `Utilisateur introuvable pour l'id : ${req.params.id}` });
+        }
+        res.status(200).json(member);
+    },
+
+    async  modifyMember(req, res) {
+        const memberId = req.params.id;
+   
+        const memberUpdates = req.body;
+        console.log("memberUpdates", memberUpdates)
+      
+        let member = await  memberModel.findById( memberId);
+      
+        // Vérifie si l'utilisateur existe
+        if (!member) {
+          return res.status(404).json({ message: "L'utilisateur n'existe pas" });
+        }
+      
+        // Met à jour les propriétés de l'utilisateur avec les valeurs fournies dans le corps de la requête
+        for (const key in memberUpdates) {
+          member[key] = memberUpdates[key];
+        }
+      
+        const updatedmember = await memberModel.update(member);
+      
+        res.json(updatedmember);
+      },
+
+      async deleteMember(req, res) {
+        const memberId = req.params.id;
+        const deletedMember = await memberModel.delete(memberId);
+      
+        if (!deletedMember)  {
+          return res.status(404).json({ message: "Membre introuvable" });
+        }
+      
+        res.json({ message: "Membre supprimé avec succès" });
+      }
+      
+}
+module.exports = memberController;
