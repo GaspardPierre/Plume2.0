a9ad59427ad20f8507b01d7f641fb39197a5978c
GaspardPierre <98175781+GaspardPierre@users.noreply.github.com>
2024-05-11 18:51:01 +0200

    Update loginController.js

--- app/controllers/loginController.js
+++ app/controllers/loginController.js
@@ -1,40 +1,45 @@
 const { memberModel } = require("../models");
 const bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken');
 require("dotenv").config();
 
 const loginControlleur = {
 
-	async checkMember(req, res) {
+    async checkMember(req, res) {
+        try {
+            console.log(req.body);
+            const { email, password } = req.body;
+            const member = await memberModel.findByEmail(email);
 
-		console.log(req.body);
-		const { email, password } = req.body;
-		const member = await memberModel.findByEmail(email);
+            if (!member) {
+                return res.status(404).json({ message: "Utilisateur non trouvé" });
+            }
 
-		if (!member) {
-			console.log(res.json);
-			return res.status(404).json({ message: "Utilisateur non trouvé" });
-		}
-		// fonction qui permet de comparer le mot de passe et le mot de passe stocqué dans la BDD pour l'itilisateur trouvé.
+            const isPasswordValid = await bcrypt.compare(password, member.password);
 
-		const isPasswordValid = await bcrypt.compare(
-			password,
-			member.password
-		);
+            if (!isPasswordValid) {
+                return res.status(401).json({ message: "Mot de passe incorrect" });
+            }
 
-		if (!isPasswordValid) {
-			return res.status(401).json({ message: "Mot de passe incorrect" });
-		}
-		// On  retire le mdp de l'objet utilisateur pas de la BDD.
-		delete member.password;
-if(member.id){
-		req.session.user ={ id : member.id};
-		req.session.role = member.role
-		console.log(`Après connexion, req.session: ${JSON.stringify(req.session)}`);
-}
-		res.status(200).json({ member });
-	},
+            const token = jwt.sign(
+                { id: member.id, role: member.role, email: member.email,pseudo: member.pseudo},
+                process.env.JWT_SECRET,
+                { expiresIn: process.env.JWT_EXPIRATION || '1h' } // Utiliser une valeur par défaut si non spécifiée
+            );
+            
+
+    
+            res.status(200).json({ message: "Connexion réussie", token });
+        } catch (err) {
+            console.error(err);
+            res.status(500).json({ error: "Une erreur est survenue lors de la vérification de l'utilisateur." });
+        }
+    },
+    async logout(req, res) {
+       
+        res.status(200).send({ message: "Déconnexion réussie. Supprimez le token côté client." });
+    }
+    
 };
-
-
 
 module.exports = loginControlleur;
